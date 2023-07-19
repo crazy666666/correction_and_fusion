809c49437a8ac4a5272e1eaa7c9b8f45a59eebbf
crazy666666 <1354696022@qq.com>
2023-07-19 09:37:59 +0800

    add1

--- correct_and_fuse.py
+++ correct_and_fuse.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from filter_adaptive_weights import Gaussian_weight,W_k
 from fast_guided_filtering import Filter
-from gamma_adaptive_weights import gamma_weight
+# from gamma_adaptive_weights import gamma_weight
 
 
 def correct_exposure(I,ksize):
@@ -12,7 +12,7 @@ def correct_exposure(I,ksize):
     w_k = np.repeat(w_k[..., None], 3, axis=-1)
     img = I.astype(float) / 255.
     guideFilter_img = Filter(img,(ksize,ksize),0.031,0.5,w_k=w_k)
-    L_refined = np.clip(guideFilter_img, 1e-3, 1) ** gamma_weight(guideFilter_img)
+    L_refined = np.clip(guideFilter_img, 1e-3, 1) ** (np.log10(0.5) / np.log10(np.mean(guideFilter_img)))
     im_corrected = img / L_refined
     img_en = np.clip(im_corrected * 255, 0, 255).astype("uint8")
 
@@ -34,3 +34,8 @@ def positive_reverse_correction(img,ksize):
     img_all_corrected = fuse_images(img, img_positive, img_reverse)
 
     return np.clip(img_all_corrected * 255, 0, 255).astype("uint8")
+
+
+if __name__ == '__main__':
+    img = cv2.imread('../1200_1200.bmp', 1)
+    img_corrected = positive_reverse_correction(img, 7)
